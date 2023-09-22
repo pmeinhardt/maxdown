56834b299de8e005219e657fa9080a4c511327da
pmeinhardt <mnhrdt@gmail.com>
2023-09-22 23:03:24 +0200

    Clean up test file

--- tests/trycmd.rs
+++ tests/trycmd.rs
@@ -1,12 +1,7 @@
 use trycmd::TestCases;
 
-const CSS: &str = include_str!("../src/github.css");
-
 #[test]
 fn trycmd() {
     let t = TestCases::new();
-
     t.case("examples/*.md");
-
-    t.extend_vars([("[CSS]", CSS)]).unwrap();
 }
